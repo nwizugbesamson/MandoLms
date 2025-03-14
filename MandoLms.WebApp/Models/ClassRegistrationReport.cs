477b5deb44b13861f03398d597f7165724ac477d
nwizugbesamson <samson6398@gmail.com>
2025-03-14 13:46:49 +0000

    create report model and service class

---
+++ MandoLms.WebApp/Models/ClassRegistrationReport.cs
@@ -0,0 +1,10 @@
+﻿namespace MandoLms.WebApp.Models
+{
+    public class ClassRegistrationReport
+    {
+        public string ClassName { get; set; }
+        public string TeacherName { get; set; }
+        public int NumberOfRegisteredStudents { get; set; }
+        public int NumberOfStudentsWithPaidFees { get; set; }
+    }
+}
---
+++ MandoLms.WebApp/Services/ClassRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿using MandoLms.WebApp.Models;
+
+namespace MandoLms.WebApp.Services
+{
+    public class ClassRegistrationService
+    {
+        public ClassRegistrationService()
+        {
+            
+        }
+
+        public IEnumerable<ClassRegistrationReport> GetRegistrationReport()
+        {
+
+        }
+    }
+}
