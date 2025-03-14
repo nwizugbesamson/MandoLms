15c84c8d11e5e847a085a4d8ebd69e41576e798b
nwizugbesamson <samson6398@gmail.com>
2025-03-14 14:18:53 +0000

    Create procedure to filter report on minimum class registrations

---
+++ MandoLms.WebApp/StoredProcedures/ClassRegistrationReportV2.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE [dbo].[ClassRegistrationReportV2]
+	@MinRegistrations INT = NULL
+AS
+BEGIN
+	SELECT
+		Cls.ClassName,
+		Tch.TeacherName,
+		COUNT(CASE WHEN ClReg.HasPaidFees = 1 THEN ClReg.Student_ID END) AS NumberOfStudentsWithPaidFees,  
+		COUNT(ClReg.Student_ID) AS NumberOfRegisteredStudents
+		FROM [dbo].[Class] Cls
+	LEFT JOIN [dbo].[Teacher] Tch
+		ON Cls.Teacher_ID = Tch.Teacher_ID
+	FULL OUTER JOIN [dbo].[ClassRegistration] ClReg
+		ON ClReg.Class_ID = Cls.Class_ID
+	LEFT JOIN [dbo].[Student] Stu
+		ON ClReg.Student_ID = Stu.Student_ID
+	GROUP BY Cls.ClassName, Tch.TeacherName
+	HAVING (@MinRegistrations IS NULL OR COUNT(ClReg.Student_ID) >= @MinRegistrations);
+END
+GO
