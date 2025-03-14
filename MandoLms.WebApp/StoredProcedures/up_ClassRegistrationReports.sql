2be7617b07d83609df1b5716f40b5f689a113fe9
nwizugbesamson <samson6398@gmail.com>
2025-03-14 13:37:02 +0000

    create stored procedure for class registration report

---
+++ MandoLms.WebApp/StoredProcedures/up_ClassRegistrationReports.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE ClassRegistrationReport
+AS
+BEGIN
+	SELECT
+		Cls.ClassName,
+		Tch.TeacherName,
+		COUNT(CASE WHEN ClReg.HasPaidFees = 1 THEN ClReg.Student_ID END) AS PaidStudents,  
+		COUNT(ClReg.Student_ID) AS TotalRegisteredStudents
+		FROM [dbo].[Class] Cls
+	LEFT JOIN [dbo].[Teacher] Tch
+		ON Cls.Teacher_ID = Tch.Teacher_ID
+	FULL OUTER JOIN [dbo].[ClassRegistration] ClReg
+		ON ClReg.Class_ID = Cls.Class_ID
+	LEFT JOIN [dbo].[Student] Stu
+		ON ClReg.Student_ID = Stu.Student_ID
+	GROUP BY Cls.ClassName, Tch.TeacherName;
+END
+GO
